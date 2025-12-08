cba67cb1156c6ecf179aa59ef45f7d7b1f57631b
Eva Intoci <evaintoci@Evas-MacBook-Air-3.local>
2025-12-07 19:10:41 -0800

    “changes”

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import About from "./pages/About";
-import SDNHM from "./pages/SDNHM"; 
+import SDNHM from "./pages/SDNHM";
+import Research from "./pages/research";
 import Contact from "./components/Contact";
 import NavBar from "./components/NavBar";
 import Line from "./components/Line";
@@ -12,14 +13,15 @@ function App() {
   return (
     <Router>
       <div className="bg-background">
-        <NavBar className="p-4" /> 
+        <NavBar className="p-4" />
         <div className="flex justify-center pt-2 pb-12">
           <Line length="95%" />
         </div>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<About />} /> 
-          <Route path="/sdnhm" element={<SDNHM />} /> 
+          <Route path="/about" element={<About />} />
+          <Route path="/sdnhm" element={<SDNHM />} />
+          <Route path="/research" element={<Research />} />
         </Routes>
         <Contact />
       </div>
@@ -28,3 +30,4 @@ function App() {
 }
 
 export default App;
+
